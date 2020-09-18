6caac9c0fcbc8f3d71af3d19ea3e4f59ed3b9eae
RuanFerreira <ruan.deathshot@gmail.com>
2020-09-17 21:45:28 -0300

    CRUD de Calçado e categoria

---
+++ app/Views/templates/dashboard/Sidemenu.php
@@ -0,0 +1,25 @@
+<div class=" container wrapper" style="height: 100%; position: absolute">
+    <!-- Sidebar -->
+    <nav id="sidebar" >
+        <div class="sidebar-header">
+            <h3>Olá ADM</h3>
+        </div>
+
+        <ul class="list-unstyled components">
+            <p> Produto </p>
+            <li class="active">
+                <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Home</a>
+                <ul class="collapse list-unstyled" id="homeSubmenu">
+                    <li>
+                        <a href="<?php echo base_url('dashboard/cadastroCalcado'); ?>">cadastrar</a>
+                    </li>
+                    <li>
+                        <a href="#">buscar</a>
+                    </li>
+                </ul>
+            </li>
+
+        </ul>
+    </nav>
+
+</div>
